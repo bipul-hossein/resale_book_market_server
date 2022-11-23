db103024ef02eea9a080e8ada7f088b558152960
Bipul-hossen <mdbipulhossain252@gmail.com>
2022-11-23 22:30:47 +0600

    first commit for checking

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.get('/', (req, res) => {
+res.send('assignment 12 is running!');
+});
+
+app.listen(port, () => {
+console.log('Example app listening on port', port);
+});
