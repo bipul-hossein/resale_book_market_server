f72c26eaca581a30d704885ccb2d29380ae4f9da
Bipul-hossen <mdbipulhossain252@gmail.com>
2022-11-25 05:28:38 +0600

    add data

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
+//middleware
 app.use(cors())
+
 app.get('/', (req, res) => {
-res.send('assignment 12 is running!');
+    res.send('assignment 12 is running!');
 });
 
 app.listen(port, () => {
-console.log('Example app listening on port', port);
+    console.log('Example app listening on port', port);
 });
+
+
+
+const uri = `mongodb+srv://${process.env.DB}:${process.env.DP}@cluster0.8gaczek.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const categoriesCollection = client.db("resaleBookAssign12").collection("categoryCollection");
+        const categoryBookCollection = client.db("resaleBookAssign12").collection("booksCollection");
+
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories)
+        })
+
+        app.get('/categories/:categoryName', async (req, res) => {
+            const category = req.params.categoryName;
+            const query = { categoryName: category }
+            const categoryBooks =await categoryBookCollection.find(query).toArray();
+            res.send(categoryBookCollection)
+        })
+
+    } finally { }
+}
+run().catch(err => console.error(err))
+
+
+
+
